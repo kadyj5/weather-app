8c35162fabe830bd4ff1deca789494830e8e0546
Karolina Dyjak <karolina.dyjak5@gmail.com>
2023-07-18 22:44:54 +0200

    Adding material design

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Typography, Container, Box } from '@mui/material';
+import {Typography, Container, Box, Card, CardContent, CardActions, Button} from '@mui/material';
 
 const API_URL =
     'https://api.open-meteo.com/v1/forecast?latitude=50.0625&longitude=19.9375&daily=temperature_2m_max,temperature_2m_min,sunrise,sunset&current_weather=true&timezone=Europe%2FBerlin&forecast_days=3';
@@ -10,10 +10,11 @@ interface WeatherData {
         temperature: number;
         windspeed: number;
         winddirection: number;
-        is_day: number
-        time: string
+        is_day: number;
+        time: string;
     };
     daily: {
+        time: string[];
         temperature_2m_max: number[];
         temperature_2m_min: number[];
         sunrise: string[];
@@ -39,31 +40,60 @@ const App: React.FC = () => {
 
     return (
         <Container maxWidth="sm">
-            <Box mt={4}>
-                <Typography variant="h4" component="h1" gutterBottom>
+            <Box mt={6} sx={{ backgroundColor: '#9BCDD2', padding: '16px', borderRadius: '8px' }}>
+                <Typography variant="h2" gutterBottom>
                     Weather App
                 </Typography>
                 {weatherData && (
-                    <div>
-                        <h2>Today's Weather</h2>
-                        <p>Time: {weatherData.current_weather.time}</p>
-                        <p>Max Temperature: {weatherData.current_weather.temperature}°C</p>
-                        <p>Wind speed: {weatherData.current_weather.windspeed}</p>
-                        <p>Wind direction: {weatherData.current_weather.winddirection}</p>
-                    </div>
+                    <Card>
+                        <CardContent>
+                            <Typography variant="h5" component="h2">
+                                Today's Weather
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                Time: {weatherData.current_weather.time}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                Temperature: {weatherData.current_weather.temperature}°C
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                Wind speed: {weatherData.current_weather.windspeed}
+                            </Typography>
+                            <Typography variant="body2" color="textSecondary">
+                                Wind direction: {weatherData.current_weather.winddirection}
+                            </Typography>
+                        </CardContent>
+                    </Card>
                 )}
 
                 {weatherData?.daily && (
                     <div>
-                        <h2>Weather Forecast</h2>
+                        <Typography variant="h5" component="h2" mt={4}>
+                            Weather Forecast
+                        </Typography>
                         {weatherData.daily.temperature_2m_max.map((temperature, index) => (
-                            <div key={index}>
-                                <p>Date: {index + 1}</p>
-                                <p>Max Temperature: {temperature}°C</p>
-                                <p>Min Temperature: {weatherData.daily.temperature_2m_min[index]}°C</p>
-                                <p>Sunrise: {weatherData.daily.sunrise[index]}</p>
-                                <p>Sunset: {weatherData.daily.sunset[index]}</p>
-                            </div>
+                            <Card key={index} variant="outlined">
+                                <CardContent>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Date: {weatherData.daily.time[index]}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Max Temperature: {temperature}°C
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Min Temperature: {weatherData.daily.temperature_2m_min[index]}°C
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Sunrise: {weatherData.daily.sunrise[index]}
+                                    </Typography>
+                                    <Typography variant="body2" color="textSecondary">
+                                        Sunset: {weatherData.daily.sunset[index]}
+                                    </Typography>
+                                </CardContent>
+                                <CardActions>
+                                    <Button size="small">More details</Button>
+                                </CardActions>
+                            </Card>
                         ))}
                     </div>
                 )}
