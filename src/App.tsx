ee910a3faa45191a53973c5c15f66bb5967d2d9c
Karolina Dyjak <karolina.dyjak5@gmail.com>
2023-07-20 21:25:34 +0200

    adding hourly data

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import React, {useEffect, useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { BrowserRouter as Router, Routes, Route, Link, useParams } from 'react-router-dom';
 import {
     Typography,
     Container,
@@ -37,7 +38,7 @@ interface WeatherData {
 
 const App: React.FC = () => {
     const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
-    const [selectedDay, setSelectedDay] = useState<number>(-1);
+    const [selectedDayHourlyData, setSelectedDayHourlyData] = useState<number[] | null>(null);
 
     useEffect(() => {
         const fetchData = async () => {
@@ -52,92 +53,130 @@ const App: React.FC = () => {
         fetchData();
     }, []);
 
+    const handleMoreDetailsClick = (index: number) => {
+        if (weatherData) {
+            const selectedDay = weatherData.daily.time[index];
+            const filteredHourlyData = weatherData.hourly.time.reduce((acc: number[], time, i) => {
+                if (time.startsWith(selectedDay)) {
+                    acc.push(weatherData.hourly.temperature_2m[i]);
+                }
+                return acc;
+            }, []);
+
+            setSelectedDayHourlyData(filteredHourlyData);
+        }
+    };
+
     return (
-        <Container maxWidth="sm">
-            <Box mt={6} sx={{ backgroundColor: '#9BCDD2', padding: '16px', borderRadius: '8px' }}>
-                <Typography variant="h1" gutterBottom>
-                    Weather App
-                </Typography>
-                {weatherData && (
-                    <Card>
-                        <CardContent>
-                            <Typography variant="h5" component="h2">
-                                Today's Weather
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Time: {weatherData.current_weather.time}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Temperature: {weatherData.current_weather.temperature}°C
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Wind speed: {weatherData.current_weather.windspeed}
-                            </Typography>
-                            <Typography variant="body2" color="textSecondary">
-                                Wind direction: {weatherData.current_weather.winddirection}
-                            </Typography>
-                        </CardContent>
-                    </Card>
-                )}
-
-                {weatherData?.daily && (
-                    <div>
-                        <Typography variant="h5" component="h2" mt={4}>
-                            Weather Forecast
-                        </Typography>
-                        {weatherData.daily.time.map((date, index) => (
-                            <Card key={index} variant="outlined">
-                                <CardContent>
-                                    <Typography variant="body2" color="textSecondary">
-                                        Date: {date}
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary">
-                                        Max Temperature: {weatherData.daily.apparent_temperature_max[index]}°C
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary">
-                                        Min Temperature: {weatherData.daily.apparent_temperature_min[index]}°C
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary">
-                                        Sunrise: {weatherData.daily.sunrise[index]}
-                                    </Typography>
-                                    <Typography variant="body2" color="textSecondary">
-                                        Sunset: {weatherData.daily.sunset[index]}
-                                    </Typography>
-                                </CardContent>
-                                <CardActions>
-                                    <Button size="small" onClick={() => setSelectedDay(index)}>
-                                        More details
-                                    </Button>
-                                </CardActions>
-                            </Card>
-                        ))}
-
-                        {selectedDay !== -1 && (
-                            <div>
-                                <Typography variant="h6" component="h3">
-                                    Hourly Data for {weatherData.daily.time[selectedDay]}
+        <Router>
+            <Container maxWidth="sm">
+                <Box mt={6} sx={{ backgroundColor: '#9BCDD2', padding: '16px', borderRadius: '8px' }}>
+                    <Typography variant="h2" gutterBottom>
+                        Weather App
+                    </Typography>
+                    {weatherData && (
+                        <Card>
+                            <CardContent>
+                                <Typography variant="h5" component="h2">
+                                    Today's Weather
                                 </Typography>
-                                {weatherData.hourly.time.map((time, index) => (
-                                    time.startsWith(weatherData.daily.time[selectedDay]) && (
-                                        <div key={index}>
-                                            <Typography variant="body2" color="textSecondary">
-                                                Time: {time}
-                                            </Typography>
-                                            <Typography variant="body2" color="textSecondary">
-                                                Temperature: {weatherData.hourly.temperature_2m[index]}°C
-                                            </Typography>
-                                            <Typography variant="body2" color="textSecondary">
-                                                Precipitation: {weatherData.hourly.precipitation[index]}mm
-                                            </Typography>
-                                        </div>
-                                    )
-                                ))}
-                            </div>
-                        )}
+                                <Typography variant="body2" color="textSecondary">
+                                    Time: {weatherData.current_weather.time}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Temperature: {weatherData.current_weather.temperature}°C
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Wind speed: {weatherData.current_weather.windspeed}
+                                </Typography>
+                                <Typography variant="body2" color="textSecondary">
+                                    Wind direction: {weatherData.current_weather.winddirection}
+                                </Typography>
+                            </CardContent>
+                        </Card>
+                    )}
+
+                    {weatherData?.daily && (
+                        <div>
+                            <Typography variant="h5" component="h2" mt={4}>
+                                Weather Forecast
+                            </Typography>
+                            {weatherData.daily.time.map((date, index) => (
+                                <Card key={index} variant="outlined">
+                                    <CardContent>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Date: {date}
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Max Temperature: {weatherData.daily.apparent_temperature_max[index]}°C
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Min Temperature: {weatherData.daily.apparent_temperature_min[index]}°C
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Sunrise: {weatherData.daily.sunrise[index]}
+                                        </Typography>
+                                        <Typography variant="body2" color="textSecondary">
+                                            Sunset: {weatherData.daily.sunset[index]}
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Link to={`/details/${index}`}>
+                                            <Button size="small" onClick={() => handleMoreDetailsClick(index)}>
+                                                More details
+                                            </Button>
+                                        </Link>
+                                    </CardActions>
+                                </Card>
+                            ))}
+                        </div>
+                    )}
+
+                    <Routes>
+                        <Route
+                            path="/details/:index"
+                            element={<DetailsScreen weatherData={weatherData} selectedDayHourlyData={selectedDayHourlyData} />}
+                        />
+                    </Routes>
+                </Box>
+            </Container>
+        </Router>
+    );
+};
+
+interface DetailsScreenProps {
+    weatherData: WeatherData | null;
+    selectedDayHourlyData: number[] | null;
+}
+
+const DetailsScreen: React.FC<DetailsScreenProps> = ({ weatherData, selectedDayHourlyData }) => {
+    const { index } = useParams<{ index: string }>();
+    const selectedDayIndex = parseInt(index || '', 10);
+
+    if (!weatherData || selectedDayIndex < 0 || selectedDayIndex >= weatherData.daily.time.length) {
+        return <div>No data available.</div>;
+    }
+
+    const selectedDay = weatherData.daily.time[selectedDayIndex];
+    const hourlyData = selectedDayHourlyData || [];
+
+    return (
+        <div>
+            <Typography variant="h4" component="h2" mt={4}>
+                Hourly Data for {selectedDay}
+            </Typography>
+            {hourlyData.length > 0 ? (
+                hourlyData.map((temperature, index) => (
+                    <div key={index}>
+                        <Typography variant="body2" color="textSecondary">
+                            Hour: {index}:00 - Temperature: {temperature}°C
+                        </Typography>
                     </div>
-                )}
-            </Box>
-        </Container>
+                ))
+            ) : (
+                <div>No hourly data available for this day.</div>
+            )}
+        </div>
     );
 };
 
